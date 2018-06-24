93752962607085044fe8d848535ae2b8a9db9f0d
hardyjoysan <hardyjoysan@gmail.com>
2018-06-24 23:18:23 +0530

    Props added to component

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App :)</h1>
         <p>This is really working!</p>
-        <Person />
-        <Person />
-        <Person />
+        <Person name="Hardy" age="28" />
+        <Person name="Aaron" age="21">My Hobbies : Racing</Person>
+        <Person name="Olivia" age="19"/>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const person = () => {
-    return <p>I'm a Person and I'm {Math.floor(Math.random() * 30)} years old!</p>
+const person = (props) => {
+    return <p>I'm a {props.name} and I'm {props.age} years old!</p>
 }
 
 export default person;
