f338738a694d32496d256ae35505028c71de06b3
hardyjoysan <hardyjoysan@gmail.com>
2018-06-24 22:38:43 +0530

    React First App Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App :)</h1>
+        <p>This is really working!</p>
+        <Person />
+      </div>
+    );
+  }
+}
+
+export default App;
